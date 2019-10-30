76d71650772a1e35c4b34d1313f5b9e0dffb2f3b
lucaspg <l.paulon97@gmail.com>
2019-10-30 00:13:31 -0300

    Fix corner case

--- ray_casting.js
+++ ray_casting.js
@@ -1,4 +1,5 @@
 const wallHeight = 64;
+const wallWidth = 64;
 const playerHeight = wallHeight / 32;
 const fieldOfView = 60;
 let playerX = 160;
@@ -9,27 +10,16 @@ const angleBetweenRays = fieldOfView / planeWidth;
 const distanceToProjectionPlane = 277; // ( planeWidth / 2 ) / tan(30º)
 
 const map = [
-    [1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 1, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1],
 ];
 
-/*
-while(true) {
-    const start = viewingAngle - (fieldOfView / 2);
-    for (let i = 0; i < planeWidth; i++) {
-        // cast ray
-        // trace until it hits awall
-        // record distance to wall
-        // increment angleBetweenRays
-    }
-}
-*/
-
 const checkWallHit = (x, y) => {
-    if ((x > 3) || (y > 3) || (x < 0) || (y < 0)) return true;
+    if ((x >= map.length) || (y >= map[0].length) || (x < 0) || (y < 0)) return true;
     return map[x][y];
 }
 
@@ -69,18 +59,21 @@ const findHorizontalIntersection = (angle) => {
 
     let x;
     if (isRayFacingRight(angle)) {
-        x = Math.floor(playerX + Math.abs(playerY - y) / Math.tan(findAngle(angle)));
+        x = playerX + (Math.abs(playerY - y) / Math.tan(findAngle(angle)));
     } else {
-        x = Math.floor(playerX - Math.abs(playerY - y) / Math.tan(findAngle(angle)));
+        x = playerX - (Math.abs(playerY - y) / Math.tan(findAngle(angle)));
     }
 
     let gridX = Math.floor(x / 64);
     let gridY = Math.floor(y / 64);
 
+    // console.log(x + ' ' + y);
+    // console.log(gridX + ' ' + gridY);
+
     wallHit = checkWallHit(gridX, gridY);
 
     const deltaY = isRayFacingUp(angle) ? -64 : 64;
-    const deltaX = isRayFacingRight(angle) ? Math.floor(64 / Math.tan(findAngle(angle))) : Math.floor(64 / Math.tan(findAngle(angle))) * (-1);
+    const deltaX = isRayFacingRight(angle) ? (64 / Math.tan(findAngle(angle))) : (64 / Math.tan(findAngle(angle))) * (-1);
 
     while (!wallHit) {
         x = x + deltaX;
@@ -88,6 +81,9 @@ const findHorizontalIntersection = (angle) => {
 
         gridX = Math.floor(x / 64);
         gridY = Math.floor(y / 64);
+
+        // console.log(x + ' ' + y);
+        // console.log(gridX + ' ' + gridY);
 
         wallHit = checkWallHit(gridX, gridY);
     }   
@@ -107,18 +103,21 @@ const findVerticalIntersection = (angle) => {
 
     let y;
     if (isRayFacingUp(angle)) {
-        y = Math.floor(playerY - Math.abs(x - playerX) * Math.tan(findAngle(angle)));
+        y = playerY - (Math.abs(x - playerX) * Math.tan(findAngle(angle)));
     } else {
-        y = Math.floor(playerY + Math.abs(x - playerX) * Math.tan(findAngle(angle)));
+        y = playerY + (Math.abs(x - playerX) * Math.tan(findAngle(angle)));
     }
 
     let gridX = Math.floor(x / 64);
     let gridY = Math.floor(y / 64);
 
+    // console.log(x + ' ' + y);
+    // console.log(gridX + ' ' + gridY);
+
     wallHit = checkWallHit(gridX, gridY);
 
     const xA = isRayFacingRight(angle) ? 64 : -64;
-    const yA = isRayFacingUp(angle) ? Math.floor(64 * Math.tan(findAngle(angle))) * (-1) : Math.floor(64 * Math.tan(findAngle(angle)));
+    const yA = isRayFacingUp(angle) ? (64 * Math.tan(findAngle(angle))) * (-1) : (64 * Math.tan(findAngle(angle)));
 
     while (!wallHit) {
         x = x + xA;
@@ -126,6 +125,9 @@ const findVerticalIntersection = (angle) => {
 
         gridX = Math.floor(x / 64);
         gridY = Math.floor(y / 64);
+
+        // console.log(x + ' ' + y);
+        // console.log(gridX + ' ' + gridY);
 
         wallHit = checkWallHit(gridX, gridY);
     }   
@@ -137,25 +139,22 @@ const findDistance = (x, y) => {
     return Math.sqrt(Math.pow(playerX - x, 2) + Math.pow(playerY - y, 2));
 }
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+const drawGrid = (map) => {
+    const rows = map.length;
+    const columns = map[0].length;
 
-var c1 = document.getElementById("3d_projection");
-var ctx1 = c1.getContext("2d");
-
-const drawGrid = () => {
-    for (let i = 1; i <= 4; i++) {
+    for (let row = 0; row <= rows; row++) {
         ctx.beginPath();
-        ctx.moveTo(0, 64 * i);
-        ctx.lineTo(320, 64 * i);
+        ctx.moveTo(0, wallWidth * row);
+        ctx.lineTo(columns * wallWidth, wallWidth * row);
         ctx.stroke();
         ctx.closePath();
     }
 
-    for (let i = 1; i <= 4; i++) {
+    for (let column = 0; column <= columns; column++) {
         ctx.beginPath();
-        ctx.moveTo(64 * i, 0);
-        ctx.lineTo(64 * i, 320);
+        ctx.moveTo(wallWidth * column, 0);
+        ctx.lineTo(wallWidth * column, rows * wallWidth);
         ctx.stroke();
         ctx.closePath();
     }
@@ -167,20 +166,71 @@ const findAngleBetween = (alpha, beta) => {
     return distance;
 }
 
-const getRightAngle = (angle) => {
+const getPositiveAngle = (angle) => {
     return (angle > 0) ? angle : 360 + angle;
 }
 
-const drawScreen = (angle) => {
-    console.log(angle);
-    ctx.clearRect(0, 0, c.width, c.height);
-    ctx1.clearRect(0, 0, c1.width, c1.height);
-    drawGrid();
-    let viewingAngle = getRightAngle(angle);
-    let startingAngle = getRightAngle(viewingAngle - 30);
+const clearCanvas = (canvas, context) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    for (let a = 0; a < 320; a ++) {
+const drawWall = (canvas, context, column, wallHeight) => {
+    context.beginPath();
+    context.moveTo(column, (canvas.height / 2) - wallHeight / 2);
+    context.lineTo(column, (canvas.height / 2) + wallHeight / 2);
+    context.strokeStyle = '#FF0000';
+    context.stroke();
+    context.closePath();
+}
+
+const drawFloor = (canvas, context, column, wallHeight) => {
+    context.beginPath();
+    context.moveTo(column, (canvas.height / 2) + wallHeight / 2);
+    context.lineTo(column, canvas.height);
+    context.strokeStyle = '#00FF00';
+    context.stroke();
+    context.closePath();
+}
+
+const drawRay = (intersection) => {
+    ctx.beginPath();
+    ctx.moveTo(playerX, playerY);
+    ctx.lineTo(intersection[0], intersection[1]);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+const drawCross = (context, canvas) => {
+    context.beginPath();
+    context.moveTo(canvas.width / 2, (canvas.height / 2) - 5);
+    context.lineTo(canvas.width / 2, (canvas.height / 2) + 5);
+    context.strokeStyle = '#FFFFFF';
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo((canvas.width / 2) - 5, canvas.height / 2);
+    context.lineTo((canvas.width / 2) + 5, canvas.height / 2);
+    context.strokeStyle = '#FFFFFF';
+    context.stroke();
+    context.closePath();
+}
+
+const drawScreen = (angle) => {
+    clearCanvas(c, ctx);
+    clearCanvas(c1, ctx1);
+
+    drawGrid(map);
+
+    let viewingAngle = getPositiveAngle(angle);
+    let startingAngle = getPositiveAngle(viewingAngle - fieldOfView / 2);
+
+    // console.log('startingAngle: ', startingAngle);
+    // console.log(playerX + ' ' + playerY);
+
+    for (let a = 319; a >= 0; a --) {
         const horizontalIntersection = findHorizontalIntersection(startingAngle);
+        // console.log('------------');
         const verticalIntersection = findVerticalIntersection(startingAngle);
 
         const horizontalDistance = findDistance(horizontalIntersection[0], horizontalIntersection[1]);
@@ -190,45 +240,68 @@ const drawScreen = (angle) => {
         let closestDistance = (horizontalDistance > verticalDistance) ? verticalDistance : horizontalDistance;
         let correctedDistance = closestDistance * Math.cos(degreesToRadians(findAngleBetween(startingAngle, viewingAngle)));
 
-        const projetedWallSlice = 64 / correctedDistance * 277;
-        ctx1.beginPath();
-        ctx1.moveTo(a, 100 - projetedWallSlice / 2);
-        ctx1.lineTo(a, 100 + projetedWallSlice / 2);
-        ctx1.strokeStyle = '#FF0000';
-        ctx1.stroke();
-        ctx1.closePath();
+        const projetedWallSlice = wallHeight / correctedDistance * distanceToProjectionPlane;
 
-        ctx.beginPath();
-        ctx.moveTo(playerX, playerY);
-        ctx.lineTo(closestIntersection[0], closestIntersection[1]);
-        ctx.stroke();
-        ctx.closePath();
+        drawWall(c1, ctx1, a, projetedWallSlice);
+        drawFloor(c1, ctx1, a, projetedWallSlice);
+        drawRay(closestIntersection);
 
-        startingAngle = (startingAngle + 60/320) % 360;
+        startingAngle = (startingAngle + fieldOfView / c1.width) % 360;
     }
+
+    drawCross(ctx1, c1);
 }
 
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var c1 = document.getElementById("3d_projection");
+var ctx1 = c1.getContext("2d");
+
 let currentAngle = 0;
+drawScreen(currentAngle % 360);
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
-        case 37:
-            currentAngle += 5;
-            drawScreen(currentAngle % 360);
-            break;
-        case 38:
+        case 87:
             playerX += Math.cos(degreesToRadians(currentAngle)) * 10;
             playerY -= Math.sin(degreesToRadians(currentAngle)) * 10;
             drawScreen(currentAngle % 360);
             break;
-        case 39:
-            currentAngle -= 5;
-            drawScreen(currentAngle % 360);
-            break;
-        case 40:
+        case 83:
             playerX -= Math.cos(degreesToRadians(currentAngle)) * 10;
             playerY += Math.sin(degreesToRadians(currentAngle)) * 10;
             drawScreen(currentAngle % 360);
             break;
     }
 };
+
+c1.requestPointerLock = c1.requestPointerLock ||
+                            c1.mozRequestPointerLock;
+
+document.exitPointerLock = document.exitPointerLock ||
+                           document.mozExitPointerLock;
+
+c1.onclick = function() {
+    c1.requestPointerLock();
+}
+
+const lockChangeAlert = () => {
+    if (document.pointerLockElement === c1 ||
+        document.mozPointerLockElement === c1) {
+        console.log('The pointer lock status is now locked');
+        document.addEventListener("mousemove", updateCamera, false);
+    } else {
+        console.log('The pointer lock status is now unlocked');
+        document.removeEventListener("mousemove", updateCamera, false);
+    }
+}
+
+const updateCamera = (e) => {
+    const turn = e.movementX;
+    currentAngle -= turn;
+    drawScreen(currentAngle % 360);
+}
+
+document.addEventListener('pointerlockchange', lockChangeAlert, false);
+document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
